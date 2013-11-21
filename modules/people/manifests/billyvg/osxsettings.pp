c44033d41e6decceaf714b3fcc55961571c188ce
Billy Vong <billyvg@gmail.com>
2013-11-21 07:41:42 -0800

    Change my dock position to right

--- modules/people/manifests/billyvg/osxsettings.pp
+++ modules/people/manifests/billyvg/osxsettings.pp
@@ -3,13 +3,14 @@ class people::billyvg::osxsettings {
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::global::disable_remote_control_ir_receiver
+
   include osx::dock::autohide
+  # Default is right
   include osx::dock::position
+  # Default is 36
+  include osx::dock::icon_size
+
   include osx::finder::unhide_library
   include osx::no_network_dsstores
   include osx::software_update
-
-  class { 'osx::dock::icon_size': 
-    size => 36
-  }
 }
