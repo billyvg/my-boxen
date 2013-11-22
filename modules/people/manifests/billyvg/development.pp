f7282999306d670fd09f602f49102710d3127f85
Billy Vong <billyvg@gmail.com>
2013-11-21 22:50:39 -0800

    Add ohmyzsh and scmbreeze

--- modules/people/manifests/billyvg/development.pp
+++ modules/people/manifests/billyvg/development.pp
@@ -1,2 +1,24 @@
 class people::billyvg::development {
+  $home = "/Users/${::boxen_user}"
+
+  repository { 'ohmyzsh':
+    source => 'robbyrussell/oh-my-zsh',
+    path   => "${home}/.oh-my-zsh",
+    require => [
+      Package['zsh']
+    ]
+  }
+git clone git://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze
+
+  repository { 'scm breeze':
+    source => 'ndbroadbent/scm_breeze',
+    path   => "${home}/.scm_breeze"
+  }
+  ~> exec { 'Install scm breeze':
+    command     => "${home}/.scm_breeze/install.sh",
+    environment => [
+      "HOME=${home}",
+    ]
+  }
+  
 }
