6b8fa26d204185bea7331b29160b114a538529c8
Billy Vong <billyvg@gmail.com>
2013-11-26 14:50:11 -0800

    Add some more personal osx settings

--- modules/people/manifests/billyvg/osxsettings.pp
+++ modules/people/manifests/billyvg/osxsettings.pp
@@ -13,4 +13,29 @@ class people::billyvg::osxsettings {
   include osx::finder::unhide_library
   include osx::no_network_dsstores
   include osx::software_update
+
+  boxen::osx_defaults { 'Enable Tap to click':
+    user   => $::boxen_user,
+    domain => 'com.apple.mouse',
+    key    => 'tapBehavior',
+    type   => 'int',
+    value  => 1
+  }
+
+  boxen::osx_defaults { 'Top Left Corner -> Start Screen Saver':
+    user   => $::boxen_user,
+    domain => 'com.apple.dock',
+    key    => 'wvous-tl-corner',
+    type   => 'int',
+    value  => 5,
+    notify => Exec['killall Dock'];
+  }
+  boxen::osx_defaults { 'Top Left Corner -> Start Screen Saver (Modifier)':
+    user   => $::boxen_user,
+    domain => 'com.apple.dock',
+    key    => 'wvous-tl-modifier',
+    type   => 'int',
+    value  => 0,
+    notify => Exec['killall Dock'];
+  }
 }
