4a1451854f49cf888de8b348ed7e6e89f8ec30c6
Billy Vong <billyvg@gmail.com>
2014-03-20 12:19:33 -0700

    Add frontend project as default for me

--- modules/people/manifests/billyvg.pp
+++ modules/people/manifests/billyvg.pp
@@ -1,8 +1,8 @@
 class people::billyvg {
   notify { 'class people::billyvg declared': }
 
+  include projects::frontend
   include people::billyvg::applications
   include people::billyvg::development
   include people::billyvg::osxsettings
-
 }
